e8c6f00f582d1523f2cf3fcf7d17146ee49df308
GEUNHO7014 <129369279+GEUNHO7014@users.noreply.github.com>
2023-06-07 16:38:48 +0900

    0607

---
+++ Vouge/js/javascript.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+// 메뉴버튼을 클릭했을때, nav와 section이 바뀌어라. 
+
+$('#wrap em').click(function(){
+
+
+$('#wrap nav').addClass('on')
+$('#wrap section').addClass('on')
+
+// 본인스스로는 서서히 사라져라.
+
+$(this).fadeOut(600) 
+
+
+
+
+
+})
+
+
+})
